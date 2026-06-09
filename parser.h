e20f261246da29acff334d4b4c2e0281b9708a6f
Matheus Brunes de Araújo <brunesaraujo@gmail.com>
2026-06-09 14:37:50 -0300

    Update parser.h

--- parser.h
+++ parser.h
@@ -4,8 +4,6 @@
 #include "token.h"
 #include "lexer.h"
 
-/* Inicializa o parser com o código-fonte e executa a análise sintática.
- * Retorna 0 se o programa for sintaticamente correto, 1 caso contrário. */
 int parse(char *codigo);
 
 #endif
